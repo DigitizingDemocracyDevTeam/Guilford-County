e7c2f3b8c7445b3e537acc61b702d9b98769bc6f
Alex Kan <lexokan@live.unc.edu>
2017-08-15 22:19:52 -0400

    Exploratory Analysis

--- scripts/readIn.py
+++ scripts/readIn.py
@@ -4,5 +4,4 @@ import matplotlib.pyplot as plt
 addressPoints = pd.read_csv('data/GIS/AddressPoints.csv', sep = ',', keep_default_na = False, low_memory = False)
 
 print(addressPoints.head())
-print(addressPoints.shape)
 
